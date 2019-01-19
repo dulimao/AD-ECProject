1afe550cb254cbec83d41393baf92557d34292ae
dlm_global <dlmazz_global@163.com>
2019-01-19 17:28:51 +0800

    初始化配置文件

---
+++ ad_core/src/main/java/com/ad/ad_core/ConfigType.java
@@ -0,0 +1,9 @@
+package com.ad.ad_core;
+
+public enum ConfigType {
+
+    API_HOST,
+    APPLICATION_CONTEXT,
+    CONFIG_READY;
+
+}
---
+++ ad_core/src/main/java/com/ad/ad_core/Configrator.java
@@ -0,0 +1,48 @@
+package com.ad.ad_core;
+
+import android.content.Context;
+
+import java.util.HashMap;
+
+public class Configrator {
+    private static final HashMap<String,Object> AD_CONFIGS = new HashMap<>();
+
+    private static class ConfigratorHolder{
+        private static Configrator instance = new Configrator();
+    }
+
+    public static Configrator getInstance(){
+        return ConfigratorHolder.instance;
+    }
+
+    private Configrator(){
+        AD_CONFIGS.put(ConfigType.CONFIG_READY.name(),false);
+    }
+
+    public void configure(){
+        AD_CONFIGS.put(ConfigType.CONFIG_READY.name(),true);
+    }
+
+    public Configrator configApiHost(String host){
+        AD_CONFIGS.put(ConfigType.API_HOST.name(),host);
+        return this;
+    }
+
+    public Configrator configContext(Context context){
+        AD_CONFIGS.put(ConfigType.APPLICATION_CONTEXT.name(),context);
+        return this;
+    }
+
+    public void checkConfigurations(){
+        boolean isReady = (boolean) AD_CONFIGS.get(ConfigType.CONFIG_READY);
+        if (!isReady){
+            throw new RuntimeException("configuration is not complete");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getConfigration(Enum<ConfigType> key){
+        checkConfigurations();
+        return (T) AD_CONFIGS.get(key.name());
+    }
+}
